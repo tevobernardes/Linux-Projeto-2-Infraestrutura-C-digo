4165fc605198cb29d3a2880b11d5bb0bc14c2e00
EstevaoSB <essbernardes@gmail.com>
2022-10-08 09:24:18 -0300

    Apache v.1.0

---
+++ shell scripts2-iac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualizando o servidor e instalando o apache2 e unzip..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
+
+
+
+
